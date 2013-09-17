e1056e94f2d22ac0b17cabf866d8bd9865d63834
Martin Gracik <martin@gracik.me>
2013-09-17 12:53:25 +0200

    String formatting requires a tuple

--- robinette/server.py
+++ robinette/server.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
     server = AsyncXMLRPCServer(('localhost', 8000), allow_none=True)
     server.add_handler(irc)
 
-    print 'Running on %s:%s' % map(str, server.server_address)
+    print 'Running on %s:%s' % tuple(map(str, server.server_address))
     try:
         server.serve_forever()
     except KeyboardInterrupt:
