5a1edb15cac470f392ccb4447b81cc99e8af2a68
Martin Gracik <martin@gracik.me>
2013-09-17 09:59:27 +0200

    Print where we're running at

--- robinette/server.py
+++ robinette/server.py
@@ -7,6 +7,8 @@ from xmlrpc.server import AsyncXMLRPCServer
 if __name__ == '__main__':
     server = AsyncXMLRPCServer(('localhost', 8000), allow_none=True)
     server.add_handler(irc)
+
+    print 'Running on %s:%s' % map(str, server.server_address)
     try:
         server.serve_forever()
     except KeyboardInterrupt:
